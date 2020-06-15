c4f4daf8a2ffea6561a91536f0ec53903c3eb060
ANUDDEEPH <32596874+Anuddeeph@users.noreply.github.com>
2020-06-16 01:58:38 +0530

    j

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
-
+<img src="ironman.JPG" alt="Anuddeeph" width="564" height="1002">
 <br />
-
 <pre>
 <?php
-
 print "Welcome to my Website\n";
 print `ifconfig`;
-
 ?>
 </pre>
-<img src="ironman.JPG" alt="Anuddeeph" width="564" height="1002">
+
